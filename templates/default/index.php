fac8e5c3e1921d524f9985c4f5e4826ceca386e6
Mathias Bynens <mathias@qiwi.be>
2012-01-12 14:05:45 +0100

    Use consistent indentation

--- templates/default/index.php
+++ templates/default/index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title><?php echo $infoscreen->title ? $infoscreen->title : "FlatTurtle"; ?></title>
-    <base href="<?php echo baseUrl(); ?>">
-    <link rel="stylesheet" href="templates/default/css/style.css" />
-
-    <style>
-    .color { background-color: <?php echo $infoscreen->color ? $infoscreen->color : "#FB8B1A"; ?>; }
-    </style>
+	<meta charset="utf-8">
+	<title><?php echo $infoscreen->title ? $infoscreen->title : "FlatTurtle"; ?></title>
+	<base href="<?php echo baseUrl(); ?>">
+	<link rel="stylesheet" href="templates/default/css/style.css" />
+	<style>
+		.color { background-color: <?php echo $infoscreen->color ? $infoscreen->color : "#FB8B1A"; ?>; }
+	</style>
 </head>
 <body>
 
@@ -34,7 +33,7 @@
 <script>
 <?php
 foreach($turtles as $turtle)
-	echo '	Turtles.grow("'.$turtle->module.'", '.json_encode($turtle->options)."); \n";
+	echo '	Turtles.grow("'.$turtle->module.'", '.json_encode($turtle->options).");\n";
 ?>
 </script>
 
