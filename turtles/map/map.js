b8171764eaa35798757de2dcff94ea0fe4fbfcfb
Jens Segers <segers.jens@gmail.com>
2012-01-19 13:01:41 +0100

    Map marker

--- turtles/map/map.js
+++ turtles/map/map.js
@@ -47,6 +47,11 @@
 			}, function(results, status) {
 				if (status == google.maps.GeocoderStatus.OK) {
 					map.setCenter(results[0].geometry.location);
+					
+					var marker = new google.maps.Marker({
+			            map: map,
+			            position: results[0].geometry.location
+			        });
 				}
 			});
 			
