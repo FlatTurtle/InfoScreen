8c797d3aa8b58d7757170f7122ae32c935300f6a
Jens Segers <segers.jens@gmail.com>
2012-02-17 18:42:16 +0100

    Twitter time ago

--- turtles/twitter/i18n/en.js
+++ turtles/twitter/i18n/en.js
@@ -1,3 +1,8 @@
 var i18n = {
-		title : "tweets"
+		title :		"tweets",
+		just_now :	"just now",
+		minutes :	"minutes ago",
+		hours :		"hours ago",
+		days :		"days ago",
+		long_time : "a long time ago"
 }
