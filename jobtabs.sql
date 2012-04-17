6959f62f5ba795960e296ebb80f4fce8b6e74507
Pieter Colpaert <pieter@iRail.be>
2012-04-17 18:53:08 +0200

    added sql for the creation of 2 tables: jobtab and jobs

---
+++ jobtabs.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 17, 2012 at 06:50 PM
+-- Server version: 5.1.61
+-- PHP Version: 5.3.6-13ubuntu3.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `flatturtle`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `jobname` varchar(30) NOT NULL,
+  `javascript` varchar(1000) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `jobs`
+--
+
+INSERT INTO `jobs` (`id`, `jobname`, `javascript`) VALUES
+(1, 'screen_on', 'application.enableScreen(true);'),
+(2, 'screen_off', 'application.enableScreen(true);'),
+(3, 'reload_jobs', 'infoscreen.reloadJobs();');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobtab`
+--
+
+CREATE TABLE IF NOT EXISTS `jobtab` (
+  `id` int(11) NOT NULL,
+  `infoscreen_id` int(11) NOT NULL,
+  `minutes` varchar(2) NOT NULL,
+  `hours` varchar(2) NOT NULL,
+  `day_of_month` varchar(2) NOT NULL,
+  `month` varchar(2) NOT NULL,
+  `day_of_week` varchar(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `infoscreen_id` (`infoscreen_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
