986ff641d18e16a50cbaaa1a1ce247c32c8d679c
Pieter Colpaert <pieter@iRail.be>
2012-04-17 19:00:07 +0200

    forgot to add a column to jobtab

--- jobtabs.sql
+++ jobtabs.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 17, 2012 at 06:50 PM
+-- Generation Time: Apr 17, 2012 at 06:59 PM
 -- Server version: 5.1.61
 -- PHP Version: 5.3.6-13ubuntu3.6
 
@@ -44,12 +44,14 @@ INSERT INTO `jobs` (`id`, `jobname`, `javascript`) VALUES
 CREATE TABLE IF NOT EXISTS `jobtab` (
   `id` int(11) NOT NULL,
   `infoscreen_id` int(11) NOT NULL,
+  `job_id` int(11) NOT NULL,
   `minutes` varchar(2) NOT NULL,
   `hours` varchar(2) NOT NULL,
   `day_of_month` varchar(2) NOT NULL,
   `month` varchar(2) NOT NULL,
   `day_of_week` varchar(2) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `infoscreen_id` (`infoscreen_id`)
+  KEY `infoscreen_id` (`infoscreen_id`),
+  KEY `job_id` (`job_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
