0cae93492740a0125bc4e66a7b4b900c4255f230
Pieter Colpaert <pieter@iRail.be>
2012-05-28 17:31:14 +0200

    Added the SQL structure for the demo infoscreens

---
+++ demo_infoscreens.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 28, 2012 at 05:30 PM
+-- Server version: 5.5.22
+-- PHP Version: 5.3.10-1ubuntu3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `flatturtle`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `demo_infoscreens`
+--
+
+CREATE TABLE IF NOT EXISTS `demo_infoscreens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `alias` varchar(255) NOT NULL,
+  `logo` varchar(255) DEFAULT NULL,
+  `color` varchar(10) DEFAULT NULL,
+  `lang` varchar(10) DEFAULT NULL,
+  `interval` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `alias` (`alias`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
