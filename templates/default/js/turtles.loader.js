3c71d48b9e5c94e7c9b086a8a8f51bf1bcd602d7
Pieter Colpaert <pieterc@member.fsf.org>
2012-06-26 14:45:20 +0200

    Workaround for 1 px fix didn't work

--- templates/default/js/turtles.loader.js
+++ templates/default/js/turtles.loader.js
@@ -53,11 +53,11 @@
 				var colspan = $(this).data('colspan');
 				
 				// damn you chrome!
-			    if(!window.chrome || $('section.group').size() % 2 != 1 || i< $('section.group').size()-1){
+//			    if(!window.chrome || $('section.group').size() % 2 != 1 || i< $('section.group').size()-1){
 			    	$(this).width( ((100 / columns) * colspan) + '%');
-			    } else if($('section.group').size() % 2 == 1){
-			    	$(this).width((Math.floor((1000 / columns) * colspan + 1)/10 ) + '%');
-			    }
+//			    } else if($('section.group').size() % 2 == 1){
+//			    	$(this).width((Math.floor((1000 / columns) * colspan + 1)/10 ) + '%');
+//			    }
 			    i++;
 			});
 		}
