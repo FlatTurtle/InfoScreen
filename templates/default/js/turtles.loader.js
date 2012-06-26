05b7150c84fbe8502deb3330235367714f16bba5
Pieter Colpaert <pieterc@member.fsf.org>
2012-06-26 14:47:44 +0200

    Workaround for 1 px fix didn't work

--- templates/default/js/turtles.loader.js
+++ templates/default/js/turtles.loader.js
@@ -54,9 +54,9 @@
 				
 				// damn you chrome!
 //			    if(!window.chrome || $('section.group').size() % 2 != 1 || i< $('section.group').size()-1){
-			    	$(this).width( ((100 / columns) * colspan) + '%');
+//			    	$(this).width( ((100 / columns) * colspan) + '%');
 //			    } else if($('section.group').size() % 2 == 1){
-//			    	$(this).width((Math.floor((1000 / columns) * colspan + 1)/10 ) + '%');
+			    	$(this).width((Math.floor((1000 / columns) * colspan + 1)/10 ) + '%');
 //			    }
 			    i++;
 			});
