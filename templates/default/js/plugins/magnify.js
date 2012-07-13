2d55e004edf791ca76cf0d23b4a71383bd84587d
Jens Segers <segers.jens@gmail.com>
2012-07-13 12:12:25 +0200

    Switcher plugin fixed

--- templates/default/js/plugins/magnify.js
+++ templates/default/js/plugins/magnify.js
@@ -6,7 +6,6 @@ var Magnify = {
 			duration = 10000;
 		
 		var element = $(".turtle#" + id);
-		var parent = element.parent;
 
 		if (element.length != 0) {
 			var parent = element.parent();
@@ -14,15 +13,27 @@ var Magnify = {
 			// switch to turtle
 			Switcher.to(id);
 			
+			// magnify turtle
+			Magnify.group(parent.attr('id'), duration);
+		}
+	},
+	
+	group : function(id, duration) {
+		// default duration
+		if (duration == undefined)
+			duration = 10000;
+		
+		var element = $('.group#' + id);
+		if (element.length != 0) {
 			$(".group").each(function() {
-				if ($(this)[0] == parent[0])
-					parent.animate({"width": "100%"});
+				if ($(this)[0] == element[0])
+					element.animate({"width": "100%"});
 				else
 					$(this).animate({"width": "0%"});
 			});
-			
-			setTimeout(Magnify.reset, duration);
 		}
+		
+		setTimeout(Magnify.reset, duration);
 	},
 	
 	reset : function() {
