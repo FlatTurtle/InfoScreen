3fbfa57e11de87a77323f30e29d5bd1cdb9df8f3
Maarten Cautreels <maarten.cautreels@gmail.com>
2012-07-20 22:39:47 +0200

    Added new icons, updated layout, icons need to get better resolution

--- turtles/villo/villo.js
+++ turtles/villo/villo.js
@@ -40,13 +40,13 @@
 		parse : function(json) {
             var villo = json.Villo;
             
-            console.log(villo.length);
             if(villo.length <= 0) {
                 return undefined;
             }
             
             for(var i in villo) {
                 villo[i].distance = Math.round(parseInt(villo[i].distance)/10)*10;
+				villo[i].name = jQuery.trim(villo[i].name);
             }
             
             return villo;
