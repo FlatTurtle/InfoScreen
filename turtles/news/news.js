ee3849335e8676e42bc5dde2fc57dda9ff73b5e0
gbostoen <gbostoen@gbostoen.(none)>
2012-08-14 17:02:59 +0200

    dynamic

--- turtles/news/news.js
+++ turtles/news/news.js
@@ -15,8 +15,11 @@
 				};
 
 				self.$el.html($.tmpl(template, data));
-				$('div#listticker').html('<div>As from the 20th of August we would like to welcome EUROFER as a new tenant to the Cortenbergh 172.</div><div>EUROFER will start its works on the first floor and part of the second floor, we apologize in case of any inconvenience and we thank you for your understanding. <p></p></div><div>Nous souhaitons la bienvenue à EUROFER, nouveau locataire du bâtiment Cortenbergh 172 à partir du 20 août.</div><div>EUROFER entamera ses travaux d’aménagements au premier étage et sur une partie du deuxième étage.</div><div>Nous vous prions de bien vouloir nous excuser des désagréments qui pourraient en résulter et nous vous remercions de votre compréhension. <p></p></div><div>Op 20 augustus heten wij EUROFER van harte welkom in Cortenbergh 172.</div><div>EUROFER zal zijn werken uitvoeren op de 1ste verdieping en een deel van de 2de verdieping.</div>');
-				//$('div#listticker').append(self.options.info);
+				var str = self.options.info;
+				str = '<div>'+ str;
+				str = str.replace(/\./g,"<br><br></div><div>");
+				if(str.charAt(str.length-1) == '>') str+='</div>';
+				$('div#listticker').html(str);
 
 				// notify listeners render completed and pass element
 				// self.trigger("rendered", self.$el);
